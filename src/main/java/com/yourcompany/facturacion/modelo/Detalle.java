1f510ecf83f8854f957aff27dc41ab7e59e02344
Samuel <samuelfelipelorente@gmail.com>
2025-01-27 19:20:58 +0100

    leccion 10

--- src/main/java/com/yourcompany/facturacion/modelo/Detalle.java
+++ src/main/java/com/yourcompany/facturacion/modelo/Detalle.java
@@ -1,6 +1,10 @@
 package com.yourcompany.facturacion.modelo;
 
+import java.math.*;
+
 import javax.persistence.*;
+
+import org.openxava.annotations.*;
 
 import lombok.*;
 
@@ -11,5 +15,12 @@ public class Detalle {
  
     @ManyToOne(fetch = FetchType.LAZY, optional = true)
     Producto producto;
+    
+    @Money
+    @Depends("producto.numero, cantidad")
+    public BigDecimal getImporte() {
+        if (producto == null || producto.getPrecio() == null) return BigDecimal.ZERO;
+        return new BigDecimal(cantidad).multiply(producto.getPrecio());
+    }
  
 }
