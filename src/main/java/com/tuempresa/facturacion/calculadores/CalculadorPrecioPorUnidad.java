c2eaea031b835cfdbbc6f9f3acc2b730dba47dbb
Samuel <samuelfelipelorente@gmail.com>
2025-01-28 09:45:59 +0100

    leccion 11

---
+++ src/main/java/com/tuempresa/facturacion/calculadores/CalculadorPrecioPorUnidad.java
@@ -0,0 +1,24 @@
+package com.tuempresa.facturacion.calculadores;
+
+
+import static org.openxava.jpa.XPersistence.getManager;
+
+import org.openxava.calculators.*;
+
+import com.yourcompany.facturacion.modelo.*;
+
+import lombok.*;
+
+public class CalculadorPrecioPorUnidad implements ICalculator {
+	 
+    @Getter @Setter
+    int numeroProducto;
+ 
+    @Override
+    public Object calculate() throws Exception {
+        Producto producto = getManager()
+            .find(Producto.class, numeroProducto);
+        return producto.getPrecio();
+    }
+ 
+}
